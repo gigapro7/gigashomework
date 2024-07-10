60e5182e1e1a54c48bc10eff20ee3378d101cba5
gigapro7 <kitokito804@gmail.com>
2024-07-11 01:34:44 +0400

    seccond commit

---
+++ day 1/homework/goal1.py
@@ -0,0 +1,22 @@
+print(5 == 2)
+print(7 == 11)
+print(3 != 15)
+print(27 != 9)
+print(20 <= 17)
+print(9 <= 7)
+print(11 >= 6)
+print(1 >= 3)
+print(7 > 8)
+print(8 < 9)
+
+
+
+
+print(False and True)
+print(False and False)
+print(True and True)
+print(True and False)
+print(False or True)
+print(True or False)
+print(False or False)
+print(True or True)
